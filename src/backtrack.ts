ed41f792d4aee59e83fac561225e6b8cf3b98657
CryptoBot <cryptobot@klinki.cz>
2023-07-11 21:17:22 +0200

    update backtracking - add state machine

--- src/backtrack.ts
+++ src/backtrack.ts
@@ -31,6 +31,7 @@ export interface CornixConfiguration {
 }
 
 export interface Order {
+  amount?: number;
   coin: string;
   leverage?: number;
   exchange?: string;
@@ -55,10 +56,10 @@ export function backtrack(config: CornixConfiguration, order: Order, data: Trade
     isFullyOpen: false,
     isFullyClosed: false,
     entries: [] as number[],
+    detailedEntries: [] as any[],
     closePrices: [] as number[],
     profits: [] as number[],
     currentSl: order.sl,
-    averageEntry: 0,
   };
 
   data.forEach(element => {
@@ -68,16 +69,22 @@ export function backtrack(config: CornixConfiguration, order: Order, data: Trade
 
     if (!results.isFullyOpen && (element.low <= remainingEntries[0].price)) {
       const openedEntry = remainingEntries[0];
-      remainingEntries.splice(0, 1);
-      results.entries.push(openedEntry.price);
 
       const spentOnEntry = config.amount * openedEntry.percentage;
       const boughtCoins = spentOnEntry / openedEntry.price;
 
       results.coins += boughtCoins;
       results.price += spentOnEntry;
-      results.averageEntry = results.entries.reduce((sum, curr) => sum + curr, 0) / results.entries.length;
 
+      results.entries.push(openedEntry.price);
+      results.detailedEntries.push({
+        entry: openedEntry.priceTarget,
+        price: openedEntry.price,
+        coins: boughtCoins,
+        total: spentOnEntry
+      });
+
+      remainingEntries.splice(0, 1);
       if (remainingEntries.length === 0) {
         results.isFullyOpen = true;
       } else if (remainingEntries[0].percentage == 0) {
@@ -95,6 +102,12 @@ export function backtrack(config: CornixConfiguration, order: Order, data: Trade
 
     if (element.high >= remainingTps[0].price) {
       const tpHit = remainingTps[0];
+
+      if (config.trailingTakeProfit !== 'without') {
+          events.push( { type: 'trailing-activated', price: tpHit.price, timestamp: element.openTime });
+          return;
+      }
+
       remainingTps.splice(0, 1);
       results.closePrices.push(tpHit.price);
 
@@ -147,3 +160,145 @@ export function mapPriceTargets(orderTargets: number[], priceTargets: PriceTarge
 
   return result;
 }
+
+interface DetailedEntry {
+  /**
+   * id of entry
+   */
+  entry: number;
+
+  /**
+   * price target of entry
+   */
+  price: number;
+
+  /**
+   * number of bought / sold coins
+   */
+  coins: number;
+
+  /**
+   * total amount of used USDT
+   */
+  total: number;
+}
+
+class StateMachine {
+  allocatedAmount = 0;
+
+  soldCoins = 0;
+  earnedProfit = 0;
+
+  waitingForEntryPrice = 0;
+  waitingForProfitPrice = 0;
+
+  currentTrailingReferencePrice = 0;
+  currentTrailingStopPrice = 0;
+
+  trailingPercentage = 0;
+  trailingActive = false;
+
+  events = [];
+
+  private results = {
+    coins: 0,
+    soldCoins: 0,
+    price: 0,
+    totalProfit: 0,
+    isFullyOpen: false,
+    isFullyClosed: false,
+    entries: [] as number[],
+    closePrices: [] as number[],
+    profits: [] as number[],
+    currentSl: 0,
+    averageEntry: 0,
+  };
+
+
+  remainingEntries = [];
+  remainingTps = [];
+
+  entries: DetailedEntry[] = [];
+  tps: DetailedEntry[] = [];
+
+  hitSl = false;
+  hitTp = false;
+
+  get boughtCoins() {
+    return this.entries.reduce((sum, curr) => sum + curr.coins, 0);
+  }
+
+  get spentAmount() {
+    return this.entries.reduce((sum, curr) => sum + curr.total, 0);
+  }
+
+  get averageEntryPrice() {
+    return  this.boughtCoins / this.spentAmount;
+  }
+
+  get isOpen() {
+    return this.entries.length > 0 && this.hitSl == false;
+  }
+
+  get isFullyOpen() {
+    return this.remainingEntries.length === 0;
+  }
+
+  get isClosed() {
+    return this.hitSl || this.remainingTps.length === 0;
+  }
+
+  constructor(public readonly order: Order, public readonly config: CornixConfiguration) {
+    this.allocatedAmount = order.amount ?? config.amount;
+  }
+
+  updateStateWithPrice(price: number) {
+    if (this.trailingActive) {
+      if (price > this.currentTrailingReferencePrice) {
+        console.log('Updating trailing price...');
+        this.currentTrailingReferencePrice = price;
+        this.currentTrailingStopPrice = price * (1 - this.trailingPercentage);
+      } else if (price < this.currentTrailingStopPrice) {
+        console.log('Closing TP point with trailing');
+        this.trailingActive = false;
+      }
+    }
+  }
+
+  hitEntryPoint(price: number) {
+    const openedEntry = remainingEntries[0];
+    remainingEntries.splice(0, 1);
+    results.entries.push(openedEntry.price);
+
+    const spentOnEntry = this.allocatedAmount * openedEntry.percentage;
+    const boughtCoins = spentOnEntry / openedEntry.price;
+
+    results.coins += boughtCoins;
+    results.price += spentOnEntry;
+    results.averageEntry = results.entries.reduce((sum, curr) => sum + curr, 0) / results.entries.length;
+
+    if (remainingEntries.length === 0) {
+      results.isFullyOpen = true;
+    } else if (remainingEntries[0].percentage == 0) {
+      results.isFullyOpen = true;
+    }
+
+    events.push({ type: 'buy', price: openedEntry.price, spent: spentOnEntry, bought: boughtCoins, timestamp: element.openTime });
+
+  }
+
+  closeTpWithPrice(price: number) {
+
+  }
+}
+
+/**
+ * Idea:
+ *
+ * State machine
+ *
+ * States:
+ * {} - initial state, waiting for entry points
+ * {e1} - after some entry, either faiting for further entries or waiting for TP/SL
+ * {e1, t1} -
+ */
